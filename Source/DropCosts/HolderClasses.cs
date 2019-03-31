fd32badc7d2cc9581d430d1a64a1797fa9f7df4d
Mad <github@typ03.de>
2019-03-31 16:16:15 +0200

    adjust tonnage limits

--- Source/DropCosts/HolderClasses.cs
+++ Source/DropCosts/HolderClasses.cs
@@ -3,11 +3,11 @@
         public float percentageOfMechCost = 0.0025f;
 
         public bool CostByTons = true;
-        public int cbillsPerTon = 2500;
+        public int cbillsPerTon = 5000;
         public bool someFreeTonnage = true;
         public int freeTonnageAmount = 0;
         public bool useFreeTonnageByDifficulty = true;
-        public int[] freeTonnageByDifficulty = new int[] { 140, 160, 180, 200, 220, 240, 260, 280, 300, 320 };
+        public int[] freeTonnageByDifficulty = new int[] { 140, 160, 180, 200, 220, 260, 280, 300, 320, 340 };
         public bool useFreeTonnagePriorityBonus = true;
         public int freeTonnagePriorityBonus = 20;
     }
