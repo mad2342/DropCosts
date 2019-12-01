f6bdaa130eb622cd6a99d684651da7b37319744c
Mad <github@typ03.de>
2019-12-01 14:44:58 +0100

    upd for BTG 1.8

--- Source/DropCosts/Properties/AssemblyInfo.cs
+++ Source/DropCosts/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("DropCosts")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1584A6A4-3528-4E13-A267-81853F569962")]
-[assembly: AssemblyVersion("1.5.0.*")]
+[assembly: AssemblyVersion("1.8.0.*")]
 
